1e576905572051a76c4502df9f639134771abc93
仇聪玫 <610271848@qq.com>
2017-12-21 08:35:52 +0800

    ss

--- src/components/registerP.vue
+++ src/components/registerP.vue
@@ -1,35 +1,58 @@
 <template>
-  <div>
-      <div class="top">
-          <p class="topI"></p>
-          <p class="topII">注册</p>
-      </div>
-  </div>
+    <div class="">
+        <div class="top">
+            <p class="topI"></p>
+            <p class="topII">注册</p>
+        </div>
+        <div class="middle">
+            <input type="number">
+            <input type="text">
+            <input type="text">
+            <input type="text">
+        </div>
+    </div>
 </template>
 
 <script>
- export default {
-      data() {
-        return {
-        };
-      },
-        methods:{
-          },
-    components: {  }
-    }
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {}
+};
 </script>
 
 <style scoped lang="less">
-.top{
-    width: 7.5rem;
-    height: .77rem;
-    background-color: #e5e5e5;
+//顶部
+.top {
+  width: 100%;
+  height: 0.77rem;
+  background-color: #e5e5e5;
+  display: flex;
+  position: relative;
 }
-.topI{
-    background: url(../assets/kehu/Sprites.png) no-repeat -15px -8px;
+.topI {
+  width: 0.5rem;
+  height: 0.5rem;
+  background: url(/static/img/Sprites.7a5be37.png) no-repeat -5px -2px;
+  position: absolute;
+  top: 0.2rem;
+  left: 0.05rem;
 }
-.topII{
-    
+.topII {
+  text-align: center;
+  font-size: 0.4rem;
+  line-height: 0.77rem;
+}
+//手机格式
+.middle {
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+  }
+  input::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+  }
 }
 </style>
 
