82f9b9462aa5e5798db1f254715603d25cacc421
suiyi <934413983@qq.com>
2017-12-22 14:05:35 +0800

    kk

--- src/router/index.js
+++ src/router/index.js
@@ -36,8 +36,8 @@ import list_list from '../views/sinda_list/list_list' //公司工商
 // 手机端
 import m_global from '../components/m_global' //手机端公共页
 import m_index from '../m_sinda/m_index' //手机端首页
-import m_success from '../m_sinda/m_success' //手机端首页
-import m_failure from '../m_sinda/m_failure' //手机端首页
+import m_success from '../m_sinda/m_success' //手机端支付成功
+import m_failure from '../m_sinda/m_failure' //手机端支付失败
 import m_shop from '../m_sinda/m_shop' //手机shop
 import m_shop_service from '../m_sinda/m_shop/m_shop_service' //手机shop service
 import m_out from '../components/m_out' //手机端公共页
@@ -45,6 +45,7 @@ import generic from '../components/generic' //手机端公共页
 import logged from '../components/logged' //手机端公共页
 import set from '../components/set' //手机端公共页
 import registerP from '../components/registerP' //手机端注册
+import m_details from '../m_sinda/m_details' //手机端商品详情
 Vue.use(Router)
 
 // pc端
@@ -184,11 +185,15 @@ export default new Router({
                 component: m_shop_service,
             }]
         }, {
+            path: 'details', // 手机端商品详情
+            name: 'm_details',
+            component: m_details,
+        }, {
             path: 'success', // 手机端的支付成功
             name: 'm_success',
             component: m_success
         }, {
-            path: 'failure', // 手机端的支付失
+            path: 'failure', // 手机端的支付失败
             name: 'm_failure',
             component: m_failure
         }]
